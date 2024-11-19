e06bc97ca5c196fc0b042b07ec16762336728798
Felipe Ehara <felipeyoug1@gmail.com>
2024-11-19 14:55:44 -0300

    Update do front-end e configuração do route (Link) da aplicação

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,14 +1,19 @@
-import { Pagina } from "@/components/template/Pagina";
-import Image from "next/image";
-import {Evento} from '@/core'
+import { Logo } from "@/components/template/LogoGrande";
+import Link from "next/link";
 
 export default function Home() {
-  
-  const e: Partial<Evento> = {};
-
   return (
-    <Pagina>
-      <div>Inicio</div>
-    </Pagina>
+    <div className="h-screen flex flex-col justify-center items-center bg-[url('/background-elementos.svg')] bg cover gap-10">
+
+      <div className="flex flex-col items-center gap-4">
+        <Logo/>
+        <p className="text-zinc-500 font-light w-96 leading-6 text-center select-none">
+          Crie e gerencie o convite do seu evento de forma rápida e facil, sem complciações!
+        </p>
+      </div>
+      <Link href="/evento" className="botao azul text-lg uppercase">
+        Crie o seu Evento!
+      </Link>
+    </div>
   );
 }
---
+++ apps/frontend/src/components/template/LogoGrande.tsx
@@ -0,0 +1,24 @@
+import { Righteous } from 'next/font/google'
+import Link from 'next/link';
+import Image from 'next/image'
+
+const fonte = Righteous({
+    subsets: ["latin"],
+    weight: "400",
+})
+
+export const Logo = () => {
+    return(
+        <Link href="/" className={`flex flex-col items-center gap-2 ${fonte.className}`}>
+            <Image 
+                src="/logo.svg" 
+                alt='logo'
+                width={100}
+                height={100}
+            />
+            <h1 className='text-5xl '>
+                CONVIT<span className='text-blue-500'>3</span> DIGITAL
+            </h1>
+        </Link>
+    );
+}
